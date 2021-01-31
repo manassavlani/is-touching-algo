653b701e2e662dfd0adfc9b3429a50f4c58f4263
manassavlani <78263712+manassavlani@users.noreply.github.com>
2021-01-31 11:54:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var movingRect,fixedRect;
+function preload() {
+  monkeyImage = loadImage("Monkey_01.png");
+}
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600,400,50,80);
+  fixedRect.shapeColor = "green";
+  monkey = createSprite(200,200,50,50);
+  monkey.addImage(monkeyImage);
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  
+}
+
+function draw() {
+  background("black"); 
+  
+  if (movingRect.x-fixedRect.x<movingRect.width/2+fixedRect.width/2
+    && fixedRect.x-movingRect.x<movingRect.width/2+fixedRect.width/2 
+    && movingRect.y-fixedRect.y<movingRect.width/2+fixedRect.width/2
+    && fixedRect.y-movingRect.y<movingRect.width/2+fixedRect.width/2){
+    movingRect.shapeColor = "red";
+    fixedRect.shapeColor = "red";
+  }
+  else {
+    movingRect.shapeColor = "green";
+    fixedRect.shapeColor = "green";
+  }
+  movingRect.x=mouseX;
+  movingRect.y=mouseY; 
+  drawSprites();
+}
